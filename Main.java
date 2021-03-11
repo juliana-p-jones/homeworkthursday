752ba311ee2985c0addaeaec45aae7844d6cd4e3
juliana jones <juliana.p.jones@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email juliana.p.jones@gmail.com>
2021-03-11 17:32:56 -0500

    commiting all files

---
+++ Main.java
@@ -0,0 +1,8 @@
+package homeworkthursday;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My top three favorite artists are: Tyler the Creator, Vampire Weekend, Tierra Whack");
+    }
+}
+//Users/julianajones/IdeaProjects/FavArtists/src/main/java/homeworkthursday/Main.java
